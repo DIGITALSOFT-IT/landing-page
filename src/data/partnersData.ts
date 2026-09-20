c08ec433eb3d09efe94bfac1e9932b653f058dfd
william <huila33@hotmail.com>
2026-09-19 20:26:54 -0500

    mejora de diseño1

--- src/data/partnersData.ts
+++ src/data/partnersData.ts
@@ -9,5 +9,6 @@ export const partnersData: Partner[] = [
   { id: 2, name: "Aliado 2", logo: "/images/partners/logo-gacetas.png" },
   { id: 3, name: "Aliado 3", logo: "/images/partners/logo-delta-solutions1.png" },
   { id: 4, name: "Aliado 4", logo: "/images/partners/logo-confianza-23.png" },
-  { id: 5, name: "Aliado 5", logo: "/images/partners/logo-uscategui.png" }
+  { id: 5, name: "Aliado 5", logo: "/images/partners/logo-uscategui.png" },
+  { id: 6, name: "Aliado ", logo: "/images/partners/logo-voz.png" }
 ];
